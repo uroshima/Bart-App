84234f1dc768a9ed24ac4ad1afdf0a4cffabeabd
urosh <urosh.stojkovikj@gmail.com>
2018-08-25 21:29:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Bart App"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
